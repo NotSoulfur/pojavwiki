c7a4f8ae8580f9b8d4af0cd1cf7331f7551dfe36
Soulfur <notsoulfur@gmail.com>
2024-12-20 14:07:29 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,79 +1,37 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import {themes as prismThemes} from 'prism-react-renderer';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
-/** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Pojavlauncher Wiki',
-  tagline: 'Funni Launcher',
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
   url: 'https://github.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/pojavwiki/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'NotSoulfur', // Usually your GitHub org/user name.
+  organizationName: 'NotSoulfur',
   projectName: 'pojavwiki',
-  deploymentBranch: 'gh-pages', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
   presets: [
     [
       'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/NotSoulfur/pojavwiki/',
         },
         blog: {
           showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/NotSoulfur/pojavwiki/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
         },
         theme: {
           customCss: './src/css/custom.css',
         },
-      }),
+      },
     ],
   ],
-
-  themeConfig:
-  ({
-    image: 'img/docusaurus-social-card.jpg',
+  themeConfig: {
     navbar: {
       title: 'My Site',
       logo: {
@@ -96,11 +54,11 @@ const config = {
           items: [
             {
               label: 'Discord',
-              href: 'https://discord.gg/pojavlauncher-724163890803638273',
+              href: 'https://discordapp.com/invite/docusaurus',
             },
             {
               label: 'X',
-              href: 'https://x.com/PLaunchTeam',
+              href: 'https://x.com/docusaurus',
             },
           ],
         },
@@ -109,7 +67,7 @@ const config = {
           items: [
             {
               label: 'GitHub',
-              href: 'https://github.com/NotSoulfur/pojavwiki/',
+              href: 'https://github.com/facebook/docusaurus',
             },
           ],
         },
@@ -120,6 +78,7 @@ const config = {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
-  }),
+  },
+};
 
 export default config;
