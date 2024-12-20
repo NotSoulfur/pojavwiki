45cce89c19104aba2849e5bc03eff8b9c0f19bc1
Soulfur <notsoulfur@gmail.com>
2024-12-20 14:53:07 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const config = {
   tagline: 'A flexible, fast and open-source Minecraft Java Edition launcher for Android and iOS',
   favicon: 'img/pojavlauncher.ico',
   url: 'https://github.com',
-  baseUrl: '/pojavwiki/intro',
+  baseUrl: '/pojavwiki/',
   organizationName: 'NotSoulfur',
   projectName: 'pojavwiki',
   deploymentBranch: 'gh-pages',
