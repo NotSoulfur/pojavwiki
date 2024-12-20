fcd63e8024e2bcaf00f69a21d382a685a83a9847
Soulfur <notsoulfur@gmail.com>
2024-12-20 13:52:34 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'NotSoulfur', // Usually your GitHub org/user name.
   projectName: 'pojavwiki',
-  deploymentBranch: 'gh-pages' // Usually your repo name.
+  deploymentBranch: 'gh-pages', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
